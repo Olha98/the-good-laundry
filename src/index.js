143cc862f2284bb116931b59bd05fb928f0901ba
Olha Hrymchak <olhahrymchak@Olhas-Mac-mini.local>
2023-06-02 18:17:52 +0300

    feat: add styles

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const element = {
+  modal: document.querySelector('.menu__box'),
+  burger: document.querySelector('#menu__btn'),
+  menu__toggle: document.querySelector('#menu__toggle'),
+};
+
+const modalModule = () => {
+  const matchTarget = e.target === element.menu__toggle;
+  document.addEventListener('click', e => {
+    openModalWindow(e);
+    closeModalWindow(e);
+  });
+
+  const openModalWindow = e => {
+    if (e.target === element.menu__toggle) {
+      document.body.style.overflow = 'hidden';
+      element.modal.classList.toggle('visible');
+    }
+  };
+
+  const closeModalWindow = e => {
+    console.log(e.target, 'tttt');
+
+    if (e.target.classList.contains('visible') || e.target.dataset.action === 'btn__closeModal' || e.key === 'Escape') {
+      console.log('hello');
+      document.body.style.overflow = 'auto';
+
+      modalOverlay.removeEventListener('click', closeModalWindow);
+      window.removeEventListener('keydown', closeModalWindow);
+    }
+  };
+};
+
+modalModule();
