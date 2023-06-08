b2cacb768b9e8c0e363006fcdb851d3f957e36b6
Olha Hrymchak <olhahrymchak@Olhas-Mac-mini.local>
2023-06-08 16:43:19 +0300

    feat: save chnages

--- src/js/AdaptiveMenu/index.js
+++ src/js/AdaptiveMenu/index.js
@@ -2,49 +2,103 @@
 
 class AdaptiveMenu extends HTMLElement {
   static initState = {
-    devise: '',
-    reset: '',
     activeDropDown: false,
-    itemsSize: 1,
-
-    menuContainerStyle: {},
-    menuListStyle: {},
-    dropZoneStyle: {},
+    dropDownList: [],
+    menuList: [],
   };
 
   constructor() {
     super();
-
+    this.gap = this.getAttribute('gap') || 30;
+    this.logo = this.getAttribute('logo');
     this.uls = Array.from(this.querySelectorAll('ul'));
     this.lis = Array.from(this.querySelectorAll('li'));
 
-    console.log('hi');
-  }
+    this.state = { ...AdaptiveMenu.initState };
 
-  static get observedAttributes() {
-    return ['logo', 'label'];
+    this.controlContainerSize();
+    this.putLogo();
+    // this.openDropDown();
+
+    this.dropDown = this.initDropDown();
+    this.dropDown.append(true);
+
+    // this.bindEvents().bind(this);
   }
 
   connectedCallback() {
-    let logo = this.getAttribute('logo');
-    this.putLogo(logo);
-
-    // let label = this.getAttribute('label');
-    // console.log(logo, label, 'LOGO');
-
     this.bindEvents();
   }
 
-  putLogo(logo) {
+  disconnectedCallback() {
+    this.bindEvents();
+  }
+
+  controlContainerSize() {
+    const { dropDownList, menuList } = this.state;
+    const widthList = this.offsetWidth;
+    let menuChild = Array.from(this.uls[0].children);
+    let widthItems = 0; // common width all items
+
+    //create label for dropDown
+    const li = document.createElement('li');
+    li.classList = 'dropDown_label';
+
+    const p = document.createElement('p');
+    let labelActiveDropDown = this.getAttribute('label');
+    p.innerHTML = labelActiveDropDown;
+
+    const span = document.createElement('span');
+    // &#9660; треугольник вниз
+    // &#9650; треугольник вверх
+    span.innerHTML = '&#9660;';
+
+    // console.log(span.textContent === '&#9650;');
+
+    p.append(span);
+    li.append(p);
+    //controlling space
+    menuChild.forEach(item => (widthItems += item.offsetWidth));
+    const spaceBetweenItems = (widthList - widthItems) / menuChild.length;
+    console.log(spaceBetweenItems, 'spaceBetweenItems');
+    if (spaceBetweenItems < this.gap) {
+      this.uls[0].append(li);
+      widthItems += li.offsetWidth;
+
+      let checkSpace = 0;
+      console.log(checkSpace, 'checkSpace');
+      menuChild.reverse().map((item, index) => {
+        widthItems -= item.offsetWidth;
+        const newSlice = menuChild.slice(0, -index).length;
+
+        checkSpace = newSlice && (widthList - widthItems) / newSlice;
+
+        if (parseInt(checkSpace.toFixed()) >= this.gap) {
+          menuList.push(item);
+        } else {
+          dropDownList.push(item);
+        }
+      });
+      this.uls[0].remove();
+      // this.uls[0].append(li)
+      console.log(this.uls[0].children);
+      console.log(menuList);
+      // this.uls[0].replaceChild(this.uls[0].children[9], this.uls[0].children);
+    }
+  }
+
+  putLogo() {
+    const { menuList } = this.state;
+
     //generate img
     const li = document.createElement('li');
     const img = document.createElement('img');
 
-    const splitImg = logo.split('.');
+    const splitImg = this.logo.split('.');
     const format = splitImg[splitImg.length - 1]; //svg
 
     const imageAtt = {
-      src: logo,
+      src: this.logo,
       alt: 'Logo',
       class: 'logo__svg',
       width: '77',
@@ -61,33 +115,81 @@ class AdaptiveMenu extends HTMLElement {
     li.appendChild(img);
 
     // find middle position and  put in center of list
-    const centerElement = Math.floor(this.lis.length / 2);
-    this.uls[0].children[centerElement].after(li);
+    console.log(menuList, 'menuList');
+    const length = menuList.length > 0 ? menuList.length : this.lis.length;
+
+    const centerElement = Math.floor(length / 2);
+
+    if (menuList.length > 0) {
+      this.uls[0].children[centerElement - 1].after(li);
+    } else {
+      this.uls[0].children[centerElement].before(li);
+    }
   }
 
-  controlSizeContainer() {}
+  initDropDown() {
+    const div = document.createElement('div');
+
+    const ul = document.createElement('ul');
+    div.classList = 'drop-down_container';
+    ul.classList = 'drop-down_list';
+
+    div.append(ul);
+
+    const hide = () => (div.style.display = 'none');
+
+    const setContent = list => {
+      list.map(item => ul.append(item));
+    };
+
+    return {
+      container: div,
+      // show: () => (div.style.display = null),
+      show: () => (div.style.display = 'none'),
+      hide,
+      append: isHide => {
+        if (isHide) {
+          hide();
+        }
+        this.after(div);
+      },
+      remove: () => div.remove(),
+      setContent,
+    };
+  }
+
+  openDropDown() {
+    const { dropDownList, activeDropDown } = this.state;
+    this.state.activeDropDown = !activeDropDown;
+
+    console.log(dropDownList, 'activeDropDown');
+
+    if (dropDownList.length > 0) {
+      this.dropDown.show();
+
+      this.dropDown.setContent(dropDownList);
+    }
+    // if (activeDropDown) {
+    //   console.log('should add');
+    //   div.style.display = 'block';
+    // } else {
+    //   console.log('should remove');
+    //   div.style.display = 'none';
+    // }
+  }
 
   bindEvents() {
+    const labelElement = this.querySelector('.dropDown_label');
+
     // as the widths are calculated, we need to resize
     // the carousel when the window is resized
     window.addEventListener('resize', () => console.log(window.innerWidth));
-    // window.addEventListener('DOMContentLoaded', this.updateSizes);
+    labelElement.addEventListener('click', this.openDropDown.bind(this));
   }
 
   updateSizes(e) {
-    console.log(e, 'e');
+    // console.log(e, 'e');
   }
 }
 
 customElements.define('adaptive-menu', AdaptiveMenu);
-
-// Container where I will controll al main settings
-// List
-// DropDown window
-
-// customElements.define('expanding-list', ExpandingList);
-
-// customElements.define('word-count', WordCount, { extends: 'p' });
-// connectedCallback - будет вызван, когда пользовательский элемент оказывается впервые встроен в DOM
-// attributeChangedCallback -вызывается, когда пользовательскому элементу добавляют, удаляют или изменяют какой-то атрибут
-//  shadowRoot.appendChild(templateContent.cloneNode(true)); - cretae template
