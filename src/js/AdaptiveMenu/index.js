15b9642887eeb53de94aa27be0e0a4a9feba6667
Olha Hrymchak <olhahrymchak@MacBook-Pro-Olha-2.local>
2023-06-12 08:49:24 +0300

    feat: save changes

--- src/js/AdaptiveMenu/index.js
+++ src/js/AdaptiveMenu/index.js
@@ -1,7 +1,51 @@
 //!  - important control container, item container, different style, and add more words
 
+class DropDown {
+  constructor(container) {
+    this.container = container;
+    // init().bind(this);
+    // append().bind(this);
+    // setContent().bind(this);
+
+    // hide().bind(this);
+    // show().bind(this);
+    // remove().bind(this);
+  }
+
+  init() {
+    const ul = document.createElement('ul');
+    this.container.classList = 'drop-down_container';
+    ul.classList = 'drop-down_list';
+    this.container.this.append(ul);
+  }
+
+  append(isHide) {
+    if (isHide) {
+      hide();
+    }
+    this.after(this.ul);
+  }
+
+  setContent = list => {
+    list.map(item => this.ul.append(item));
+  };
+
+  hide() {
+    this.container.style.display = 'none';
+  }
+  show() {
+    this.container.style.display = 'block';
+  }
+  remove() {
+    this.container.remove();
+  }
+}
+
+// const dropDown = new DropDown();
+
 class AdaptiveMenu extends HTMLElement {
   static initState = {
+    // dropDown: new DropDown(),
     activeDropDown: false,
     dropDownList: [],
     menuList: [],
@@ -16,14 +60,13 @@ class AdaptiveMenu extends HTMLElement {
 
     this.state = { ...AdaptiveMenu.initState };
 
-    this.controlContainerSize();
-    this.putLogo();
-    // this.openDropDown();
+    // this.controlContainerSize();
 
+    // this.openDropDown();
+    this.controlContainerSize(this.offsetWidth);
+    this.putLogo();
     this.dropDown = this.initDropDown();
     this.dropDown.append(true);
-
-    // this.bindEvents().bind(this);
   }
 
   connectedCallback() {
@@ -34,9 +77,11 @@ class AdaptiveMenu extends HTMLElement {
     this.bindEvents();
   }
 
-  controlContainerSize() {
+  controlContainerSize(width) {
+    console.log(width, 'width');
     const { dropDownList, menuList } = this.state;
-    const widthList = this.offsetWidth;
+    // const widthList = this.offsetWidth;
+    const widthList = width;
     let menuChild = Array.from(this.uls[0].children);
     let widthItems = 0; // common width all items
 
@@ -51,16 +96,18 @@ class AdaptiveMenu extends HTMLElement {
     const span = document.createElement('span');
     span.innerHTML = '&#9660;';
 
-    // console.log(span.textContent === '&#9650;');
-
     p.append(span);
     li.append(p);
     //controlling space
     menuChild.forEach(item => (widthItems += item.offsetWidth));
     const spaceBetweenItems = (widthList - widthItems) / menuChild.length;
-    console.log(spaceBetweenItems, 'spaceBetweenItems');
+
+    // console.log(spaceBetweenItems, 'spaceBetweenItems');
 
     if (spaceBetweenItems <= this.gap) {
+      console.log('NO SPACE');
+      // console.log(this.uls[0].children, '(this.uls[0].children');
+
       this.uls[0].append(li);
       widthItems += li.offsetWidth;
 
@@ -68,24 +115,63 @@ class AdaptiveMenu extends HTMLElement {
 
       menuChild.reverse().map((item, index) => {
         widthItems -= item.offsetWidth;
+
         const newSlice = menuChild.slice(0, -index).length;
 
-        checkSpace = newSlice && (widthList - widthItems) / newSlice;
+        console.log(checkSpace, 'checkSpace');
 
-        if (parseInt(checkSpace.toFixed()) >= this.gap) {
-          menuList.push(item);
-        } else {
+        if (parseInt(checkSpace.toFixed()) <= Number(this.gap)) {
+          checkSpace = newSlice && (widthList - widthItems) / newSlice;
+
           this.uls[0].removeChild(item);
           dropDownList.push(item);
+        } else {
+          menuList.push(item);
         }
       });
+    } else {
+      let widthDynamic = 0;
+      const spaceBetweenItems = (widthList - widthDynamic) / menuList.length;
 
-      // menuChild.map(item => item.removeChild(item));
-      // console.log(this.uls[0].children);
-      console.log(menuChild), 'menuChild';
+      console.log(spaceBetweenItems, 'spaceBetweenItems');
+      console.log(menuList, 'menuList');
+
+      if (spaceBetweenItems >= this.gap) {
+        dropDownList.map(item => {
+          // widthDynamic += item.offsetWidth;
+          this.uls[0].append(item);
+          console.log('OOOOOO');
+          // menuList.push(item);
+        });
+      }
+
+      if (dropDownList.length === 0) {
+        const label = document.querySelector('.dropDown_label');
+        label && label.remove();
+      }
+
+      // item.style.display = 'inline-block';
+      // this.uls[0].removeChild();
+      // li.style.background = 'none';
+      // li.remove();
+      // p.remove();
+      // console.log(saveNav, 'saveNav');
+      // console.log('aalalqlalqllq');
+      // console.log(this.uls[0].children, 'HERE!');
     }
-    // this.uls[0].childNodes = [];
-    // }
+
+    // let commonWidth = 0;
+    // const nav = [];
+
+    // menuChild.map(item => {
+    //   console.log(commonWidth, 'commonWidth');
+    //   if (commonWidth <= this.gap) {
+    //     commonWidth += item.offsetWidth + this.gap;
+    //     nav.push(item);
+    //   }
+    // });
+    // console.log(this.lis, 'this.lis');
+    // console.log(nav, 'hghgh');
   }
 
   putLogo() {
@@ -173,15 +259,16 @@ class AdaptiveMenu extends HTMLElement {
 
   bindEvents() {
     const labelElement = this.querySelector('.dropDown_label');
-
+    // console.log(header__container, 'header__container');
     // as the widths are calculated, we need to resize
     // the carousel when the window is resized
-    window.addEventListener('resize', () => console.log(window.innerWidth));
-    labelElement.addEventListener('click', this.toggleDropDown.bind(this));
-  }
-
-  updateSizes(e) {
-    // console.log(e, 'e');
+    //  const widthList = this.offsetWidth;
+    window.addEventListener('resize', e => {
+      const width = this.offsetWidth;
+      this.controlContainerSize(width);
+    });
+    // window.addEventListener('resize', () => this.updateSizes.bind(this));
+    labelElement?.addEventListener('click', () => this.toggleDropDown());
   }
 }
 
